8cd9fae914faed40cc164ce17a95c0c45aa2d483
Alivewwt <m13032536099@163.com>
2016-11-08 21:53:50 +0800

    leetcode
    leetcode

---
+++ leetcode/binaryTreeZigzagLevel.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    //奇数行节点不需逆置
+    //偶数行节点逆置
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> res=new ArrayList<>();
+        if(root==null)
+        return res;
+        Queue<TreeNode> queue=new LinkedList<TreeNode>();
+        queue.offer(root);
+        boolean rev=false;
+        while(!queue.isEmpty()){
+            int size=queue.size();
+            ArrayList<Integer> level=new ArrayList<Integer>();
+            for(int i=0;i<size;i++){
+                TreeNode t=queue.poll();
+                level.add(t.val);
+                if(t.left!=null)
+                    queue.offer(t.left);
+                    if(t.right!=null)
+                    queue.offer(t.right);
+            }
+            if(rev){
+                 Collections.reverse(level);
+            //res.add(level);
+               rev=false;
+            }
+           else rev=true;
+             res.add(level);
+        }
+        return res;
+    }
+}
