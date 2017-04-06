ba36634840bf15312c81484cf16320cc7494f9b7
Alivewwt <m13032536099@163.com>
2017-04-06 21:57:57 +0800

    leetcode
    leetcode

---
+++ leetcode/reverse.java
@@ -0,0 +1,42 @@
+package project.leetcode;
+
+//交换位置
+public class reverse {
+	public static int reverse(int x){
+		if(x>Integer.MAX_VALUE||x<Integer.MIN_VALUE)
+			return 0;
+		String s=String.valueOf(x);
+		if(s.length()==1)
+			return x;
+		char ch=s.charAt(0);
+		int a,b;
+		String str="";
+		long res = 0;		
+		x=Math.abs(x);
+		while(x!=0){
+			 b=x%10;
+			 res=res*10+b;
+			 x/=10;
+		}
+		if(ch=='-'){
+			res=-res;
+		}
+		
+		if(res>Integer.MAX_VALUE||res<Integer.MIN_VALUE)
+			return 0;
+		return (int)res;
+	}
+	/*
+	 * 	long res = 0;
+		int b;
+		while(x!=0){
+			 b=x%10;
+			 res=res*10+b;
+			 x/=10;
+		}
+		
+		if(res>Integer.MAX_VALUE||res<Integer.MIN_VALUE)
+			return 0;
+		return (int)res;
+	 */
+}
